5ec6230a29120f72b2ad4af676aa4aa5898a87dc
Jesper Kristensen <mail@jesperkristensen.dk>
2012-09-26 18:21:32 +0200

    Bug 792721 - MPL 2 upgrade: l10n for da

--- extensions/spellcheck/hunspell/extension.sh
+++ extensions/spellcheck/hunspell/extension.sh
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 mkdir dictionaries
 mv da.dic dictionaries/da.dic
 mv da.aff dictionaries/da.aff
