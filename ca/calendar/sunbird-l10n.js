1625769b9d40866ea1c7ff3837c8ebe499a4dd98
Eduard Gamonal <edu@eduard-gamonal.net>
2013-10-16 22:31:18 +0200

    Jordi Serratosa's work

--- ca/calendar/sunbird-l10n.js
+++ ca/calendar/sunbird-l10n.js
@@ -6,7 +6,7 @@
 
 # the default day to start the week on
 #0=Sunday 1=Monday 2=Tuesday 3=Wednesday 4=Thursday 5=Friday 6=Saturday
-pref("calendar.week.start", 1);
+pref("calendar.week.start", 0);
 
 # default days off (not in work week)
 pref("calendar.week.d0sundaysoff", true);
@@ -17,7 +17,7 @@ pref("calendar.week.d4thursdaysoff", false);
 pref("calendar.week.d5fridaysoff", false);
 pref("calendar.week.d6saturdaysoff", true);
 
-pref("general.useragent.locale", "ca");
+pref("general.useragent.locale", "@AB_CD@");
 
 # categories
-pref("calendar.categories.names", "Aniversari,Negocis,Trucades,Clients,Competència,Consumidors,Preferits,Seguiments,Regals,Vacances,Idees,Assumptes,Miscel·lània,Personal,Projectes,Dia de festa,Estat,Proveïdors,Viatge,Descans");
+pref("calendar.categories.names", "Anniversary,Birthday,Business,Calls,Clients,Competition,Customer,Favorites,Follow up,Gifts,Holidays,Ideas,Issues,Meeting,Miscellaneous,Personal,Projects,Public Holiday,Status,Suppliers,Travel,Vacation");
